5b80489f39c0f9a79851ef828ffe77a108be5c86
Turing Zhu <qishiwenjun@163.com>
2018-06-20 15:47:37 +0800

    init project

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("功能: 比对翻译的2018-edition和原书2018-edition中commit hash");
+    println!("1. 获取原书文件列表");
+    println!("2. 根据文件列表获取原书文件commit hash 并和翻译的进行比对");
+	println!("3. 注意如果文件不存在也要打印出来, 注意如果不匹配则要 打印出来");
+}
